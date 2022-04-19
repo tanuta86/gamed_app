d32c481ce9521f1bb6874b5b8af63ef58ad7e545
tanuta86 <t.s.a.tahpkar@gmail.com>
2022-04-19 09:05:00 +0000

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,8 @@ test "email validation should accept valid addresses" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
