00466f1a39c286e10e173cf8af042f7c6f4a9f61
tanuta86 <t.s.a.tahpkar@gmail.com>
2022-04-26 13:08:31 +0000

    13.35

--- test/controllers/self_introductions_controller_test.rb
+++ test/controllers/self_introductions_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class SelfIntroductionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @self_introduction = self_introductions(:orange)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'SelfIntroduction.count' do
+      post self_introductions_path, params: { self_introduction: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'SelfIntroduction.count' do
+      delete self_introduction_path(@self_introduction)
+    end
+    assert_redirected_to login_url
+  end
 end
