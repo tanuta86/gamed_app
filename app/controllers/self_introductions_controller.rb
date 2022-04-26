6d73181c09d4bac644294782f8dc98fd13512efd
tanuta86 <t.s.a.tahpkar@gmail.com>
2022-04-26 13:44:16 +0000

    13.45

--- app/controllers/self_introductions_controller.rb
+++ app/controllers/self_introductions_controller.rb
@@ -2,9 +2,21 @@ class SelfIntroductionsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @self_introduction = current_user.build_self_introduction(self_introduction_params)
+    if @self_introduction.save
+      flash[:success] = "自己紹介文作成完了!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
-end
 
+  private
+
+    def self_introduction_params
+      params.require(:self_introduction).permit(:content)
+    end
+end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
+  
   def home
+    @self_introduction = current_user.build_self_introduction if logged_in?
   end
+
 
   def contact
   end
