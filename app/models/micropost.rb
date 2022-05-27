afd4a915e78f9661f0d2a74ad013eae43eaa24b0
tanuta86 <t.s.a.tahpkar@gmail.com>
2022-05-27 11:06:39 +0000

    a

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,12 +1,18 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   belongs_to :group
-  has_one_attached :image
+  
+  if Rails.env.production?
+    has_one_attached :image, service: :s3
+  else  
+    has_one_attached :image
+  end
+
   default_scope -> { order(created_at: :desc) }
   validates :user_id,  presence: true
   validates :group_id, presence: true
   validates :content,  presence: true, length: { maximum: 1000 }
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png image/jpg],
                                       message: "must be a valid image format" },
                       size:         { less_than: 5.megabytes,
                                       message: "should be less than 5MB" }
